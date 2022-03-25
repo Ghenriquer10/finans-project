803c67149aa1f4950eccb7ac39bf454375f1bfc9
Gustavo Henrique Nogueira <ghenriquer10@gmail.com>
2022-03-25 19:26:52 -0300

    middle-content

--- src/components/ContentMiddle/index.js
+++ src/components/ContentMiddle/index.js
@@ -1,7 +1,16 @@
+import * as C from './style';
+import about from '../../assets/saiba.png'
 export default function ContentMiddle(){
     return(
-        <>
-            <h1>Meio do conteúdo</h1>
-        </>
+        <C.Container>
+            <C.FirstContent>
+                <div className='about'>
+                    <h1>Saiba para onde vai o seu dinheiro!</h1>
+                    <p>Com o FINANS, você categoriza todos os seus lançamentos, com gráficos simples, você sabe de onde vem e para onde vai o seu dinheiro.</p>
+                    <button>Saiba mais...</button>
+                </div>
+                <div className='about-image'><img src={about} alt='saiba mais'/></div>
+            </C.FirstContent>
+        </C.Container>
     )
 }
--- src/components/ContentMiddle/style.js
+++ src/components/ContentMiddle/style.js
@@ -1,1 +1,33 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display:  flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+    border: 1px solid red;
+`;
+
+export const FirstContent = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    flex-direction: row;
+    border: 1px solir red;
+    padding: 100px;
+
+    .about{
+        width: 100%;
+        height: 300px;
+    }
+    .about-image{
+        width: 100%;
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+`;
+
